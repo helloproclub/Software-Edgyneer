7b864b3287a44dc4f04d7ec8d33e55eac00f86f0
zarszz <zarszzntunx@gmail.com>
2019-02-07 16:26:49 +0700

    Create main.cpp
    snake game - kroco
    created with c++
    i use my c++ old style

---
+++ main.cpp
@@ -0,0 +1,161 @@
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+
+
+using namespace std;
+bool gameOver;
+const int width = 20;
+const int heigth = 20;
+int x,y,fruitX,fruitY,score;
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN};
+eDirection dir;
+int tailX[100], tailY[100];
+int nTail;
+
+void Setup()
+{
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = heigth / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % heigth;
+}
+void Draw()
+{
+    system("cls"); //system ("clear");
+    for (int i=0; i<width+2; i++)
+    {
+        cout<<"#";
+    }
+    cout<<endl;
+
+    for(int i=0; i<heigth; i++)
+    {
+        for(int j=0;j<width; j++)
+        {
+            if(j==0)
+                cout<<"#";
+            if(i==y&&j==x)
+                cout<<"0";
+            else if(i==fruitY && j==fruitX)
+                cout<<"F";
+            else
+            {   bool print = false;
+                for(int k = 0; k < nTail; k++)
+                {
+
+                    if(tailX[k] == j && tailY[k] == i)
+                    {
+                        cout<<"o";
+                        print = true;
+                    }
+
+                }
+                 if(!print)
+                      cout<<" ";
+
+            }
+            if(j==width-1)
+                cout<<"#";
+                   }
+            cout<<endl;
+    }
+    for (int i=0; i<width+2; i++)
+    {
+        cout<<"#";
+    }
+    cout<<endl;
+    cout<<"score : "<<score<<endl;
+
+}
+void Input()
+{
+    if(_kbhit())
+    {
+        switch (_getch())
+        {
+        case 'a':
+            dir = LEFT;
+            break;
+        case 'd' :
+            dir = RIGHT;
+            break;
+        case 'w' :
+            dir = UP;
+            break;
+        case 's' :
+            dir = DOWN;
+            break;
+        case 'x' :
+            gameOver = true;
+            break;
+
+        }
+    }
+}
+void Logic()
+{
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+    for(int i=1; i<nTail; i++)
+    {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
+    switch(dir)
+    {
+    case LEFT :
+        x--;
+        break;
+    case RIGHT :
+        x++;
+        break;
+    case UP :
+        y--;
+        break;
+    case DOWN :
+        y++;
+        break;
+    default :
+        break;
+    }
+    
+    if(x >= width)  x=0; else if(x<0) x=width - 1;
+    if(y >= heigth) y=0; else if(y<0) y=heigth - 1;
+
+    for(int i=0; i<nTail; i++)
+    {
+        if(tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+
+    }
+    if(x==fruitX && y==fruitY)
+    {
+        score += 10;
+        fruitX = rand() % width;
+        fruitY = rand() % heigth;
+        nTail++;
+    }
+}
+int main()
+{
+    Setup();
+    while(!gameOver)
+    {
+        Draw();
+        Input();
+        Logic();
+        Sleep(90); //sleep(10);
+    }
+    return 0;
+}
